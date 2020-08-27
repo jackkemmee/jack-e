39209637424b4358a183f0e27991e7b0fa7eae82
ronnayote <jackkemmee@gmail.com>
2020-08-27 14:19:21 +0700

    commit

---
+++ jack-e/src/components/Profile.vue
@@ -0,0 +1,21 @@
+<template>
+<h1>
+    {{msg}}
+</h1>
+  
+</template>
+
+<script>
+export default {
+   name: 'Profile',
+   data: function () {
+       return {
+            msg: "This is my profile component"
+       }
+   }
+}
+</script>
+
+<style>
+
+</style>
